b651c45d330848247550768438973beb810491e8
Ankur <ankurrathore4u@gmail.com>
2024-03-26 22:46:17 +0000

    add new test for course

--- src/test/java/com/cts/testcases/CreateCourseTest.java
+++ src/test/java/com/cts/testcases/CreateCourseTest.java
@@ -1,10 +1,9 @@
 package com.cts.testcases;
 
-public class Sample {
+public class CreateCourseTest {
 
 	public static void main(String[] args) {
-		
-		System.out.println("This is sample method");
+		// TODO Auto-generated method stub
 
 	}
 
